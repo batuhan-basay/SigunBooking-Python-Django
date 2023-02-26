e9ce1504dca36f7b98712f3b00c475bac9ad8f27
BatuhanBaşay <batuhan.basay@hotmail.com>
2023-02-26 23:31:25 +0300

    create templates and models
    create base.html, index.html, static files. Create users class in models

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    username = models.CharField(max_length=50)
+    full_name = models.CharField(max_length=50)
+    mail = models.EmailField(max_length=50)
+    password = models.CharField(max_length=50)
+    image= models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateField()
+    
